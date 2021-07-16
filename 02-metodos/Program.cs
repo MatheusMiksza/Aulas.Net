d71b4cad331740225e077ef0ecdde6f3dc2c11b6
User <User@DESKTOP-SM5S33N>
2021-07-16 12:51:51 -0300

    Adicionar arquivos de projeto.

---
+++ 02-metodos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C#-examples/Revisao/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iforme a opção desejada");
+            Console.WriteLine("1- Insira o nome do aluno: ");
+            Console.WriteLine("2- Listar alunos: ");
+            Console.WriteLine("3- Calcular média geral ");
+            Console.WriteLine("4- Sair");
+            Console.WriteLine("");
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario != "4")
+            {
+                switch(opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicionar usuario
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        break;
+                    case "3":
+                        //TODO: média geral
+                        break;
+                }
+            }
+          
+        }
+    }
+}
